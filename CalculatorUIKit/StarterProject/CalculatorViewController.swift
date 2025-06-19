1e27596d3074bf9198c2534aee8ceba627a2ae7d
Patrickalvares <patrickalvaresrj@gmail.com>
2025-06-19 16:58:24 -0300

    Refactor CalculatorViewController and update Main.storyboard for button configurations
    - Added displayNumber computed property to retrieve the current number from the display label.
    - Introduced operationIsSelected property to check if an operation button is selected.
    - Fixed typo in the operation case from 'sutract' to 'subtract'.
    - Enhanced button setup logic using forEach for better readability.
    - Updated button action methods to handle operation logic and display updates more effectively.
    - Improved decimal and clear button functionalities.
    - Adjusted Main.storyboard to refine operator button configurations and states.

--- CalculatorUIKit/StarterProject/CalculatorViewController.swift
+++ CalculatorUIKit/StarterProject/CalculatorViewController.swift
@@ -2,6 +2,11 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
     
+    var displayNumber: Double {
+        let text = displayLabel.text!
+        let number = Double(text)!
+        return number
+    }
     
     @IBOutlet var roundButtons: [UIButton]!
     
@@ -19,13 +24,26 @@ class CalculatorViewController: UIViewController {
     enum Operation {
         case divide
         case multiply
-        case sutract
+        case subtract
         case add
         case none
     }
     
     var operation: Operation = .none
+
     
+    var operationIsSelected: Bool {
+        
+        for button in operationButtons {
+            if button.isSelected {
+                return true
+            }
+        }
+        return false
+    }
+    
+    var previusNumber: Double? = 0
+    var equalButtonTapped: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,17 +53,26 @@ class CalculatorViewController: UIViewController {
     
     func setupButtons(){
     
-        for button in operationButtons {
+        operationButtons.forEach  {button in
+            
            button.layer.cornerRadius =  button.frame.height / 2
+           button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
         }
     
-        for button in roundButtons{
+        roundButtons.forEach { button in
             button.layer.cornerRadius = button.frame.height / 2
+            button.titleLabel?.font = UIFont.systemFont(ofSize: 30)
         }
-    
+        equalButton.layer.cornerRadius = equalButton.frame.height / 2
+        equalButton.titleLabel?.font = UIFont.systemFont(ofSize: 30)
     }
 
     @IBAction func didTapOperationButton(_ sender: OperatorButton) {
+        
+        if let _ = previusNumber, !equalButtonTapped, !operationIsSelected{
+            performOperation()
+            self.previusNumber = nil
+        }
         let title = sender.currentTitle
         switch title {
         case "÷":
@@ -53,20 +80,26 @@ class CalculatorViewController: UIViewController {
         case "X":
             operation = .multiply
         case "-":
-            operation = .sutract
+            operation = .subtract
         case "+":
             operation = .add
         default : break
         }
         highlightButton(sender)
+        equalButtonTapped = false
+        previusNumber = displayNumber
     }
     
     
     func deselectButton()
-    {for button in operationButtons{
-        button.backgroundColor = .systemOrange
-        button.setTitleColor(.white, for: .normal)
-    }}
+    {
+        operationButtons.forEach{ button in
+            button.backgroundColor = .systemOrange
+            button.setTitleColor(.white, for: .normal)
+            button.isSelection = false}
+        
+    }
+    
     func highlightButton(_ button: OperatorButton){
         deselectButton()
         button.backgroundColor = .white
@@ -77,22 +110,83 @@ class CalculatorViewController: UIViewController {
     @IBAction func didTapNumberButton(_ sender: UIButton) {
         let number = sender.tag
         
-        if displayLabel.text == "0" {
+        if operationIsSelected{
+            deselectButton()
             displayLabel.text = "\(number)"
-        } else {
-            displayLabel.text! += "\(number)"
+        }else{
+            if displayNumber == 0 {
+                displayLabel.text = "\(number)"
+            } else {
+                displayLabel.text! += "\(number)"
+            }
         }
     }
+    
+    func performOperation(){
+        guard let previusNumber else { return }
+        
+        var result: Double = 0
+        
+        switch operation {
+        case .divide:
+            result = previusNumber / displayNumber
+        case .multiply:
+            result = previusNumber * displayNumber
+        case .subtract:
+            result = previusNumber - displayNumber
+        case .add:
+            result = previusNumber + displayNumber
+        case .none:
+            return
+        }
+        if result.truncatingRemainder(dividingBy: 1) == 0  {
+            let int = Int(result)
+            displayLabel.text = "\(int)"
+        }else{
+            displayLabel.text = "\(result)"
+        }
+        
+        displayLabel.text = "\(result)"
+        self.previusNumber = result
+    }
+    
     @IBAction func didTapDecimalButton(_ sender: UIButton) {
+        let text = displayLabel.text!
+        if text.last != "." {
+            displayLabel.text!.removeLast()
+        } else if !text.contains(".") {
+            displayLabel.text! += "."
+        }
     }
 
-    @IBAction func didTapEqualButton(_ sender: UIButton) {
+    @IBAction func didTapEqualButton() {
+        guard operation != .none else { return }
+        performOperation()
+        equalButtonTapped = true
     }
+    
     @IBAction func didTapPercentButton() {
+        guard var n = Double(displayLabel.text!) else{
+            return
+        }
+        n /= 100
+        displayLabel.text = "\(n)"
     }
+    
     @IBAction func didTapPlusMinusBtton() {
+        guard var n = Double(displayLabel.text!) else{
+            return
+        }
+        n *= -1
+        
+        displayLabel.text = "\(n)"
     }
+    
     @IBAction func didTapClearButton() {
+        previusNumber = nil
+        displayLabel.text = "0"
+        operation = .none
+        deselectButton()
     }
     
 }
